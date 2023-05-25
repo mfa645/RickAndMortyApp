0efbb77ad91fa8da590950d261cf8ec349b612a6
Mariano Fernandez <mfa645@inlumine.ual.es>
2023-05-25 14:39:50 +0200

    Quotes service done

--- frontend/rick-and-morty-app/src/app/quotes/quotes.component.ts
+++ frontend/rick-and-morty-app/src/app/quotes/quotes.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { ScrapperService } from '../services/scrapper.service';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
   styleUrls: ['./quotes.component.scss']
 })
-export class QuotesComponent {
+export class QuotesComponent implements OnInit {
+quotes! : String[];
+quote :String= "";
+constructor(private quoteService : ScrapperService){}
+
+
+ngOnInit(){
+this.quoteService.getQuotes().subscribe((response)=>{
+  this.quotes = response;
+})
+console.log(this.quotes);
+}
+
+onGenerateQuote(){
+  this.quote=this.quotes[Math.floor(Math.random() * this.quotes.length)]
+}
 
 }
